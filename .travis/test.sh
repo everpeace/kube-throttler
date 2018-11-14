d04e012bebcc7a67974543ba53beb46f1f351031
Shingo Omura <everpeace@gmail.com>
2018-11-15 01:34:06 +0900

    fix .travis/test.sh not to fail on release(set version) commit

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,7 +4,8 @@ set -e
 sbt ++${TRAVIS_SCALA_VERSION} test
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    echo "$DOCKER_PASSWORD" | docker login -u everpeace --password-stdin
-    grep "SNAPSHOT" version.sbt >/dev/null 2>&1 && \
+    if grep "SNAPSHOT" version.sbt >/dev/null 2>&1; then
+        echo "$DOCKER_PASSWORD" | docker login -u everpeace --password-stdin
         sbt ++${TRAVIS_SCALA_VERSION} docker:stage docker:publish
+    fi
 fi
